0be090c9d0246d19e9c356c78bcc5feb090307c4
Nicole Chung <nicole.chung@gmail.com>
2022-09-19 11:45:32 -0400

    changed files to blank for the starter

--- src/background.js
+++ src/background.js
@@ -1,17 +1,1 @@
-import browser from "webextension-polyfill";
-
-async function getTab() {
-  const queryOptions = { active: true, currentWindow: true };
-  const [firstTab] = await browser.tabs.query(queryOptions);
-  return firstTab;
-}
-
-browser.tabs.onUpdated.addListener(function () {
-  getTab().then((tab) => {
-    console.log({ tab });
-    browser.scripting.executeScript({
-      target: { tabId: tab.id, allFrames: true },
-      files: ["content_script.js"],
-    });
-  });
-});
+console.log("hello from the background script");
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Popup } from "./Popup";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Popup />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+console.log("main loaded");
